8c2c2fd429f588bdd2ba027323a60915e5230ba3
Marcelo Gelato <gelatomarcelo@hotmail.com>
2024-10-04 20:37:40 -0300

    design changes

---
+++ js/main.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const toggleButton = document.getElementById('mode-toggle');
+    const body = document.body;
+
+    toggleButton.addEventListener('click', function () {
+        if (body.classList.contains('clear-mode')) {
+            body.classList.replace('clear-mode', 'dark-mode');
+            toggleButton.textContent = 'Modo claro';
+        } else {
+            body.classList.replace('dark-mode', 'clear-mode');
+            toggleButton.textContent = 'Modo oscuro';
+        }
+    });
+});
